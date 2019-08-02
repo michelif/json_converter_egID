4628c7957b8299c1c9b9b29e4cbde5da493bc119
michelif <francesco.micheli@cern.ch>
2019-08-02 11:15:45 +0200

    adding id name

--- rootConversionUtils.py
+++ rootConversionUtils.py
@@ -1,13 +1,20 @@
 import ROOT,json 
 from collections import OrderedDict
+import os
 
-def writeJSON(sfDict,filename):
-    json.dump(sfDict,filename,sort_keys=False,indent=4)
+def writeJSON(sfDict,f):
+    json.dump(sfDict,f,sort_keys=False,indent=4)
 
+def updateJSON(sfDict,f):
+    f_1 =  open(f.name,'r')
+    print(f.name)
+    json_data = json.load(f_1)
+    json_data.dump(sfDict,f_1,sort_keys=False,indent=4)
 
 def readFile(filename, th2name, idname, datasetname):
-    sfDict = OrderedDict() #it keeps the order in which elements are inserted into the dictionary
-
+    idDict = OrderedDict() #OrderedDict keeps the order in which elements are inserted into the dictionary
+    sfDict = OrderedDict() 
+    
     print('opening ', filename)
     print('get th2: ',th2name)
     rootfile = ROOT.TFile.Open(filename)
@@ -19,18 +26,19 @@ def readFile(filename, th2name, idname, datasetname):
     etaAxis = th2.GetXaxis()
     ptAxis = th2.GetYaxis()
     
+    
+    sfDict[idname]=OrderedDict()
     for iPt in range(1,nPtBins+1):# do i need +1?
-        sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"] = OrderedDict()
+        sfDict[idname]["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"] = OrderedDict()
         for iEta in range(1,nEtaBins+1):
-            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"] = OrderedDict()
-            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["value"] = th2.GetBinContent(iEta,iPt)
-            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["error"] = th2.GetBinError(iEta,iPt)                
+            sfDict[idname]["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"] = OrderedDict()
+            sfDict[idname]["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["value"] = th2.GetBinContent(iEta,iPt)
+            sfDict[idname]["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["error"] = th2.GetBinError(iEta,iPt)                
 
     for x,y in sfDict.items():
         print(x,y)
         
     f = open(datasetname+str("_")+idname+str(".json"),'w')
-
     writeJSON(sfDict,f)
 
     
