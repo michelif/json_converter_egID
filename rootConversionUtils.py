bf20797511d5e30032bbf0853cd31a1e874103b5
michelif <francesco.micheli@cern.ch>
2019-08-02 10:37:56 +0200

    adding ordered dict

--- rootConversionUtils.py
+++ rootConversionUtils.py
@@ -1,7 +1,12 @@
-import ROOT 
+import ROOT,json 
+from collections import OrderedDict
+
+def writeJSON(sfDict,filename):
+    json.dump(sfDict,filename,sort_keys=False,indent=4)
+
 
 def readFile(filename, th2name, idname, datasetname):
-    sfDict = {}
+    sfDict = OrderedDict() #it keeps the order in which elements are inserted into the dictionary
 
     print('opening ', filename)
     print('get th2: ',th2name)
@@ -15,12 +20,17 @@ def readFile(filename, th2name, idname, datasetname):
     ptAxis = th2.GetYaxis()
     
     for iPt in range(1,nPtBins+1):# do i need +1?
+        sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"] = OrderedDict()
         for iEta in range(1,nEtaBins+1):
-            print(ptAxis.GetBinLowEdge(iPt),etaAxis.GetBinLowEdge(iEta))
-            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"] = {}
-            for iEta in range(1,nEtaBins):
-                sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"] = th2.GetBinContent(iPt,iEta)
-                
+            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"] = OrderedDict()
+            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["value"] = th2.GetBinContent(iEta,iPt)
+            sfDict["pt:["+str(ptAxis.GetBinLowEdge(iPt))+","+str(ptAxis.GetBinUpEdge(iPt))+"]"]["eta:["+str(etaAxis.GetBinLowEdge(iEta))+","+str(etaAxis.GetBinUpEdge(iEta))+"]"]["error"] = th2.GetBinError(iEta,iPt)                
 
     for x,y in sfDict.items():
         print(x,y)
+        
+    f = open(datasetname+str("_")+idname+str(".json"),'w')
+
+    writeJSON(sfDict,f)
+
+    
