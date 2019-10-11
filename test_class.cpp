2e699c8c242fbbe102bb8bde5d661230810948c7
cesare borgia <prinzvinz@gmx.at>
2019-10-11 17:13:40 +0200

    ready for PR

--- test_class.cpp
+++ test_class.cpp
@@ -5,12 +5,12 @@ using namespace std;
 // TESTING
 int main () {
     SF_Reader sf;
-    sf.read_json("run2_eleIDs.json");
+    sf.read_json("jsons/run2_eleIDs.json");
 
-    cout << "2016, cutBasedElectronID-Fall17-94X-V2-loose, pt:[20.0,35.0], eta:[1.444,1.566], error: ";
-    cout << sf.error ("2016", "cutBasedElectronID-Fall17-94X-V2-loose", "pt:[20.0,35.0]", "eta:[1.444,1.566]") << endl;
+    cout << "2017, mvaEleID-Fall17-noIso-V2-wp80, pt:[20.0,35.0], eta:[1.444,1.566], error: ";
+    cout << sf.error ("2017", "mvaEleID-Fall17-noIso-V2-wp80", "pt:[20.0,35.0]", "eta:[1.444,1.566]") << endl;
 
-    cout << "2016, cutBasedElectronID-Fall17-94X-V2-loose, pt:[100.0,200.0], eta:[2.0,2.5, value:: "; 
-    cout << sf.value ("2016", "cutBasedElectronID-Fall17-94X-V2-loose", "pt:[100.0,200.0]", "eta:[2.0,2.5]")   << endl;
+    cout << "2017, mvaEleID-Fall17-noIso-V2-wp80, pt:[100.0,200.0], eta:[2.0,2.5], value: "; 
+    cout << sf.value ("2017", "mvaEleID-Fall17-noIso-V2-wp80", "pt:[100.0,200.0]", "eta:[2.0,2.5]")   << endl;
     return 0;
 }
